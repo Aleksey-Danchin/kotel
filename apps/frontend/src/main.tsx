e280b7698b65438dee7196137f71b52df16a2b42
AlekseyDanchin <aleksey.danchin@gmail.com>
2026-03-20 07:19:45 +0300

    Добавил tanstack: query + router

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { router } from "./global/router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./global/queryClient";
+import { getRoot } from "./global/getRoot";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(getRoot()).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
